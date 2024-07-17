4b7277d40ed25c74d6ee52d48580dacd0d2434c9
caBreRaS14 <97133878+caBreRaS14@users.noreply.github.com>
2024-07-17 01:26:56 -0500

    Funcion para crear productos lista

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const boton = document.querySelector(".btn");
 const calc = document.querySelector(".calcular");
 const mostar = document.querySelector(".Buscar");
-
-let total = 0;
-let contador = 0;
+const crear = document.querySelector(".crear");
 
 let array = [];
 
@@ -23,19 +21,10 @@ class Productos{
             Nombre: ${this.nom}
             Precio: ${this.pre}
             Cantidad: ${this.cant}`);
-      }
-
-    cantidad(){
-        let sumatoria = this.cant; 
-        array.push(sumatoria);
-        console.log(array)
     }
 
-    
+
 }
-
-
-
 
 array.push(new Productos("Sony",38.81, 45));
 array.push(new Productos("Lenovo",45.81, 12));
@@ -50,24 +39,29 @@ array.push(new Productos("Vivo",42.81, 36));
 
 console.log(array);
 
+//mostar los productos
+
 boton.addEventListener("click", ()=>{
     array.forEach(elemento => elemento.mostrarInfo());
     
 })
 
-
-
+//calcular la sumatoria de los productos
 
 calc.addEventListener("click", () =>{
+    let total = 0;
+    let contador = 0;
     for (let i = 0; i<array.length; i++){
         contador = array[i].pre*array[i].cant;
         total = total + contador; 
-        
     }
-
-    console.log("El total de todos los produsctos es: "+total);
+    console.log("El total de todos los produsctos es: "+ total);
 
 });
+
+
+
+//buscar los productos
 
 let nom;
 let nombres =[];
@@ -79,30 +73,52 @@ for (let i = 0; i<array.length; i++){
 let nomSave = nombres.join(`
     Nombre: `);
 
-
-
 mostar.addEventListener("click", () =>{
     let respuesta = prompt(`Escribe el nombre del elemento que deceas buscar:
         Nombre: ${nomSave} 
         `);
     buscarProducto(respuesta)
 
-    function buscarProducto (datoUsuario){
+})
 
-        const encontrar = array.find((j) => j.nom === datoUsuario);
+function buscarProducto (datoUsuario){
+    const encontrar = array.find((j) => j.nom === datoUsuario);
+    if(encontrar === undefined){
+        console.log("producto no encontrado");
+    }else{
         nameProdu = encontrar.nom;
         precioProdu = encontrar.pre;
         cantidadProdu = encontrar.cant;
-        console.log(`
-                Nombre: ${nameProdu}
-                Precio: ${precioProdu}
-                Cantidad: ${cantidadProdu}`)
 
+        console.log(`
+            Nombre: ${nameProdu}
+            Precio: ${precioProdu}
+            Cantidad: ${cantidadProdu}`)
 
     }
+    
 
+    
+}
 
+//agregar productos
+
+crear.addEventListener("click", ()=>{
+    let newname = prompt("ingrese el nombre del ceular");
+    let newprecio = prompt("ingrese el valor");
+    let newcantidad = prompt("ingrese la cantidad");
+
+    array.push(new Productos(newname, newprecio, newcantidad));
 })
+
+
+//Elmininar productos
+
+
+
+
+
+
 
 
 
