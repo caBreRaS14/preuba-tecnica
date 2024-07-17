172efa65500ad55ff7270d7262d7c5e2731118cc
caBreRaS14 <97133878+caBreRaS14@users.noreply.github.com>
2024-07-16 23:55:12 -0500

    funciones para sumar cantidades y datos creada funcion para buscar por nombre creado

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const boton = document.querySelector(".btn");
-const calc = document.querySelector(".calcular")
+const calc = document.querySelector(".calcular");
+const mostar = document.querySelector(".Buscar");
+
+let total = 0;
+let contador = 0;
 
 let array = [];
+
+let nameProdu ;
+let precioProdu; 
+let cantidadProdu; 
 
 class Productos{
     constructor(nombre, precio, cantidad){
@@ -10,48 +18,100 @@ class Productos{
         this.cant = cantidad
     }
 
-    mostarInfo(){
-        console.log(`El inventario disponible es 
-            marca: ${this.nom}
-            precio: ${this.pre}
-            cantidad en stock: ${this.cant}`);
-    }
+    mostrarInfo(){
+        console.log(`
+            Nombre: ${this.nom}
+            Precio: ${this.pre}
+            Cantidad: ${this.cant}`);
+      }
 
     cantidad(){
         let sumatoria = this.cant; 
         array.push(sumatoria);
         console.log(array)
     }
+
+    
 }
 
-const productos1 = new Productos("Sony",38.81, 45)
-const productos2 = new Productos("Lenovo",45.81, 12)
-const productos3 = new Productos("amsung",52.81, 45)
-const productos4 = new Productos("Philips",63.81, 75)
-const productos5 = new Productos("alcatel",74.81, 65)
-const productos6 = new Productos("Dell",88.81, 78)
-const productos7 = new Productos("Motorol",96.81, 36)
-const productos8 = new Productos("Nokia",45.81, 17)
-const productos9 = new Productos("Apple",42.81, 32)
-const productos10 = new Productos("Sony",42.81, 36)
 
+
+
+array.push(new Productos("Sony",38.81, 45));
+array.push(new Productos("Lenovo",45.81, 12));
+array.push(new Productos("Samsung",52.81, 45));
+array.push(new Productos("Philips",63.81, 75));
+array.push(new Productos("alcatel",74.81, 65));
+array.push(new Productos("Dell",88.81, 78));
+array.push(new Productos("Motorol",96.81, 36));
+array.push(new Productos("Nokia",45.81, 17));
+array.push(new Productos("Apple",42.81, 32));
+array.push(new Productos("Vivo",42.81, 36));
+
+console.log(array);
 
 boton.addEventListener("click", ()=>{
-    productos1.mostarInfo()
-    productos2.mostarInfo()
-    productos3.mostarInfo()
-    productos4.mostarInfo()
-    productos5.mostarInfo()
-    productos6.mostarInfo()
-    productos7.mostarInfo()
-    productos8.mostarInfo()
-    productos9.mostarInfo()
-    productos10.mostarInfo()
+    array.forEach(elemento => elemento.mostrarInfo());
+    
 })
 
-calc.addEventListener("click", ()=>{
-    productos1.cantidad();
+
+
+
+calc.addEventListener("click", () =>{
+    for (let i = 0; i<array.length; i++){
+        contador = array[i].pre*array[i].cant;
+        total = total + contador; 
+        
+    }
+
+    console.log("El total de todos los produsctos es: "+total);
+
+});
+
+let nom;
+let nombres =[];
+for (let i = 0; i<array.length; i++){
+    nom = array[i].nom;
+    nombres.push(nom);
+}
+
+let nomSave = nombres.join(`
+    Nombre: `);
+
+
+
+mostar.addEventListener("click", () =>{
+    let respuesta = prompt(`Escribe el nombre del elemento que deceas buscar:
+        Nombre: ${nomSave} 
+        `);
+    buscarProducto(respuesta)
+
+    function buscarProducto (datoUsuario){
+
+        const encontrar = array.find((j) => j.nom === datoUsuario);
+        nameProdu = encontrar.nom;
+        precioProdu = encontrar.pre;
+        cantidadProdu = encontrar.cant;
+        console.log(`
+                Nombre: ${nameProdu}
+                Precio: ${precioProdu}
+                Cantidad: ${cantidadProdu}`)
+
+
+    }
+
+
 })
+
+
+
+
+
+
+
+
+
 
 
 
